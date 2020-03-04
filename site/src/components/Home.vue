3e858a5a99d0732472119af725a63fcc2a506263
Alex Valois <alex.b.valois@gmail.com>
2020-03-03 20:46:58 -0600

    init site

---
+++ site/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Under Construction</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #de8512;
+}
+</style>
