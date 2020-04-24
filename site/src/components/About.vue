87abe6825790f9faddb33310ddaa7cc442d94a90
ABValois <alex.b.valois@gmail.com>
2020-04-23 22:11:34 -0500

    this site page

--- site/src/components/About.vue
+++ site/src/components/About.vue
@@ -6,19 +6,31 @@
             <b-col> 
                 <b-card>
                     <h1>Octatoniq? What does that mean?</h1><br>
-                    <b-card> {{name_bio}} </b-card>
+                    <b-card> {{name_bio}} 
+                        Octatoniq is the creative front run by Alex Valois.
+                        Board games, music, technology - everything Alex does, Octatoniq does.
+                    </b-card>
                 </b-card><br>
                 <b-card>
                     <h1>Okay, so what is it?</h1><br>
-                    <b-card> {{ocatatoniq_bio}} </b-card>
+                    <b-card> 
+                        Octatoniq is the creative front run by Alex Valois.
+                        Board games, music, technology - everything Alex does, Octatoniq does.
+                    </b-card>
                 </b-card><br>
                 <b-card>
                     <h1>Who is this Alex guy?</h1><br>
-                    <b-card> {{alex_bio}} </b-card>
+                    <b-card> 
+                        Alex got his Bachelor of Science from the University of Wisconsin-Madison, majoring in Computer Engineering, Computer Science, and Percussion Performance.
+                        He now works as an Information Security Engineer, and spends his free time pursuing his many interests. 
+                    </b-card>
                 </b-card><br>
                 <b-card>
                     <h1>So what can I do on this site?</h1><br>
-                    <b-card> {{alex_bio}} </b-card>
+                    <b-card>
+                        Alex got his Bachelor of Science from the University of Wisconsin-Madison, majoring in Computer Engineering, Computer Science, and Percussion Performance.
+                        He now works as an Information Security Engineer, and spends his free time pursuing his many interests. 
+                    </b-card>
                 </b-card><br>
             </b-col>
             <b-col>
@@ -29,24 +41,3 @@
         </b-row>
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            name_bio: "\
-                    Octatoniq is the creative front run by Alex Valois.\
-                    Board games, music, technology - everything Alex does, Octatoniq does.\
-                    ",
-            ocatatoniq_bio: "\
-                    Octatoniq is the creative front run by Alex Valois.\
-                    Board games, music, technology - everything Alex does, Octatoniq does.\
-                    ",
-            alex_bio: "\
-                    Alex got his Bachelor of Science from the University of Wisconsin-Madison, majoring in Computer Engineering, Computer Science, and Percussion Performance.\
-                    He now works as an Information Security Engineer, while still spending his free time pursuing his many interests.\
-                    ",
-        }
-    }
-}
-</script>
--- site/src/components/ThisSite.vue
+++ site/src/components/ThisSite.vue
@@ -1,8 +1,38 @@
 
 <template>
-  <div>
-    <h1>This Site</h1>
-    https://github.com/ABValois/octatoniq
+  <div style="padding:20px">
+    <b-card>
+      <h1>Wanna learn how to make your own website?</h1><br>
+
+      An example can be a big help! I have all code for both making this site and deploying it in
+      <a href="https://github.com/ABValois/octatoniq">a repository on Github.</a>
+      Here is <a href="https://github.com/ABValois/octatoniq/blob/master/site/src/components/ThisSite.vue">the page you are reading right now!</a>
+      <br><br>
+
+      The site itself is powered by the <a href="https://vuejs.org/">Vue.js framework</a> amd uses 
+      <a href="https://bootstrap-vue.org/">Bootstrap</a> heavily to make things look nice.
+      <br><br>
+
+      To host the site, I utilize Amazon Web Services. I use <a href="https://aws.amazon.com/s3/">S3</a> to host the website files, and 
+      <a href="https://aws.amazon.com/cloudfront/">Cloudfront</a>  to serve them.
+      <br><br>
+
+      Finally, when deploying the site, I use <a href="https://www.terraform.io/">Terraform</a>, which uses a simple declarative language to read 
+      and deploy all the hosting infrastructure from <a href="https://github.com/ABValois/octatoniq/tree/master/terraform">code stored in the site repository.</a>
+      <br><br>
+
+      All told, this costs me around ~$20 a year ($12 of which goes to registering my domain name), and I get complete control over every part of the site! 
+      <br><br>
+
+      It can be daunting to tackle a project like hosting a site when you have never done something like that before, but don't worry: there are 
+      <a href="https://www.google.com/search?q=host+vue+site+on+cloudfront">PLENTY of resources to help you along on the internet.</a>
+      <br>
+      I learned most of this myself by googling things while I was in college and a junior engineer. But lucky you! You found this site, and my example, 
+      and you can start by simply downloading it and trying to run it yourself!
+      <br><br>
+
+      Once you have my example running, you can play around with things and make it suit your needs better/figure out how things work. Happy websiting!
+    </b-card>
   </div>
 </template>
 
